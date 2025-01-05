18cb19e288a3df0270936aec323713c115ebee39
latikaguptak <08guptal@gmail.com>
2025-01-05 23:19:16 +0530

    update the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-gray-200 py-8 mt-32 bottom-0 w-full">
-      {/* Main Footer Content */}
+      
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6">
-        {/* Company Info */}
+  
         <div>
           <img
             src="https://image.similarpng.com/very-thumbnail/2022/01/Medical-health-logo-design-on-transparent-background-PNG.png"
@@ -18,7 +18,7 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Quick Links */}
+        
         <div>
           <h3 className="text-lg font-semibold mb-4 text-white">Quick Links</h3>
           <ul className="space-y-2">
@@ -28,24 +28,24 @@ const Footer = () => {
               </a>
             </li>
             <li>
-              <a href="/products" className="hover:text-blue-400">
+              <a href="#" className="hover:text-blue-400">
                 Products
               </a>
             </li>
             <li>
-              <a href="/careers" className="hover:text-blue-400">
+              <a href="#" className="hover:text-blue-400">
                 Careers
               </a>
             </li>
             <li>
-              <a href="/contact" className="hover:text-blue-400">
+              <a href="#" className="hover:text-blue-400">
                 Contact Us
               </a>
             </li>
           </ul>
         </div>
 
-        {/* Contact Info */}
+        
         <div>
           <h3 className="text-lg font-semibold mb-4 text-white">Contact Us</h3>
           <p className="text-sm mb-2">
@@ -66,10 +66,10 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Divider */}
+    
       <div className="border-t border-gray-600 mt-8"></div>
 
-      {/* Bottom Section */}
+    
       <div className="container mx-auto mt-4 flex flex-col sm:flex-row justify-between items-center px-6 text-sm">
         <p className="mb-4 sm:mb-0">© 2025 PharmaCo. All rights reserved.</p>
         <div className="flex space-x-4">
@@ -82,7 +82,7 @@ const Footer = () => {
           </a>
           <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
             <img
-              src="https://upload.wikimedia.org/wikipedia/commons/6/60/Twitter_Logo_as_of_2021.svg"
+              src="https://www.logo.wine/a/logo/Twitter/Twitter-Logo.wine.svg"
               alt="Twitter"
               className="h-6 w-6 hover:opacity-80"
             />
